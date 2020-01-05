b0d512e4f5507f0094906c0a22942531ad43a774
trnglina <contact@trnglina.org>
2020-01-05 10:29:45 -0700

    Shrink image sizes

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@ USER=sysadmin
 HOST=trnglina.org
 DIR=/var/www/trnglina.org/public_html
 
-gulp build && hugo && rsync -avz --delete public/ ${USER}@${HOST}:${DIR}
+gulp build && hugo && rsync -Iavz --delete public/ ${USER}@${HOST}:${DIR}
