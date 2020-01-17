2b78099275100897e0ab8e1f0f2844216582c598
trnglina <contact@trnglina.org>
2020-01-17 08:28:47 -0700

    Various changes

--- static/scripts/scale-post-card.js
+++
@@ -1,2 +0,0 @@
-"use strict";!function(){for(var e=document.getElementsByClassName("post-card"),t=0;t<e.length;t++){400<e[t].getElementsByTagName("h1")[0].clientHeight&&e[t].classList.add("small-heading")}}();
-//# sourceMappingURL=scale-post-card.js.map
