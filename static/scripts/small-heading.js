51fae4cac8227422f7853bee486a09f9ba2354f9
trnglina <contact@trnglina.org>
2020-01-18 08:04:36 -0700

    Remove built files

--- static/scripts/small-heading.js
+++
@@ -1,2 +0,0 @@
-"use strict";!function(){var t=document.getElementsByClassName("post-card");function e(){for(var e=0;e<t.length;e++){var n=window.innerWidth<768?300:400;t[e].getElementsByTagName("h1")[0].clientHeight>n&&t[e].classList.add("small-heading")}}window.addEventListener("resize",function(){return e()}),e()}();
-//# sourceMappingURL=small-heading.js.map
