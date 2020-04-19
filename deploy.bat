ca99cadbfbc8d334c252b39cc416554dfc91fd8f
Teng Liu <teng@trnglina.org>
2020-04-19 13:59:09 -0600

    Windowsfy deploy script

---
+++ deploy.bat
@@ -0,0 +1,1 @@
+gulp build && hugo && wsl rsync -Iavz --delete public/ sysadmin@trnglina.org:/var/www/trnglina.org/public_html
--- deploy.sh
+++
@@ -1,5 +0,0 @@
-USER=sysadmin
-HOST=trnglina.org
-DIR=/var/www/trnglina.org/public_html
-
-gulp build && hugo && rsync -Iavz --delete public/ ${USER}@${HOST}:${DIR}
