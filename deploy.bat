090c01b2bd5e601dfba48e464818a96c0af5d507
Teng Liu <teng@trnglina.org>
2020-06-06 10:56:36 -0600

    Updates

--- deploy.bat
+++ deploy.bat
@@ -1,1 +1,1 @@
-gulp build && hugo && wsl rsync -Iavz --delete public/ sysadmin@trnglina.org:/var/www/trnglina.org/public_html
+git submodule update --init --recursive && gulp build && hugo && wsl rsync -Iavz --delete public/ sysadmin@trnglina.org:/var/www/trnglina.org/public_html
