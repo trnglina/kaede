ac20be29393e7bbaefa98a22223c927d2c7f9500
Teng Liu <teng@trnglina.org>
2020-09-06 15:52:45 -0600

    Prevent JS error when there are no footnotes

--- scripts/inline-footnotes.js
+++ scripts/inline-footnotes.js
@@ -4,34 +4,36 @@ function getCSSVariable(key) {
 
 function updateInlineFootnotes() {
   const container = document.getElementsByClassName('footnotes')[0]
-  const bar = container.querySelector('hr')
-  const notes = container.querySelectorAll('li')
+  if (container) {
+    const bar = container.querySelector('hr')
+    const notes = container.querySelectorAll('li')
 
-  if (window.innerWidth > parseInt(getCSSVariable('--two-columns'))) {
-    bar.style.display = 'none'
+    if (window.innerWidth > parseInt(getCSSVariable('--two-columns'))) {
+      bar.style.display = 'none'
 
-    const refs = document.getElementsByClassName('footnote-ref')
-    for (let i = 0, acc = 0; i < notes.length; i++) {
-      const note = notes[i]
-      const offset = refs[i].parentElement.offsetTop + 1
+      const refs = document.getElementsByClassName('footnote-ref')
+      for (let i = 0, acc = 0; i < notes.length; i++) {
+        const note = notes[i]
+        const offset = refs[i].parentElement.offsetTop + 1
 
-      note.style.width = getCSSVariable('--col-right-width')
-      note.style.position = 'absolute'
-      note.style.right = 0
-      note.style.margin = 0
-      note.style.top = (offset > acc ? offset : acc) + 'px'
+        note.style.width = getCSSVariable('--col-right-width')
+        note.style.position = 'absolute'
+        note.style.right = 0
+        note.style.margin = 0
+        note.style.top = (offset > acc ? offset : acc) + 'px'
 
-      if (i == 0) {
-        acc += note.offsetTop
+        if (i == 0) {
+          acc += note.offsetTop
+        }
+        acc += note.clientHeight + parseInt(getCSSVariable('--article-block-margin'))
       }
-      acc += note.clientHeight + parseInt(getCSSVariable('--article-block-margin'))
-    }
-  } else {
-    bar.removeAttribute('style')
+    } else {
+      bar.removeAttribute('style')
 
-    for (let i = 0; i < notes.length; i++) {
-      const note = notes[i]
-      note.removeAttribute('style')
+      for (let i = 0; i < notes.length; i++) {
+        const note = notes[i]
+        note.removeAttribute('style')
+      }
     }
   }
 }
